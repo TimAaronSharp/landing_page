822d340a84b724b9e244842f38fa516c5c578831
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-07-23 17:17:21 -0600

    CREATED: assets/audio and added audio files. assets/css and moved style.css into it for organization. index.html - <audio id="audio-player"> to play audio. app.js - playAudio().

--- app.js
+++ app.js
@@ -13,4 +13,37 @@ function skillAnimator() {
   })
 }
 
+function playAudio() {
+  const audioPlayer = document.getElementById('audio-player')
+  const playButton = document.getElementById('audio-play-button')
+  const playlist = [
+    'assets/audio/something_need_doing.mp3',
+    'assets/audio/dabu.mp3',
+    'assets/audio/zug_zug.mp3',
+    'assets/audio/work_work.mp3',
+    'assets/audio/me_busy.mp3',
+    'assets/audio/leave_me_alone.mp3',
+    'assets/audio/no_time_for_play.mp3'
+  ]
+
+  let currentPlaylistIndex = 0
+
+  // Without this check audioPlayer.src is flagged with an error because getElementById returns a generic HTMLElement, which doesn't have a src attribute.
+  if (audioPlayer instanceof HTMLAudioElement) {
+
+    playButton.addEventListener('click', () => {
+      audioPlayer.src = playlist[currentPlaylistIndex]
+      audioPlayer.play()
+
+      currentPlaylistIndex++
+
+      if (currentPlaylistIndex >= playlist.length) {
+        currentPlaylistIndex = 0
+      }
+
+    })
+  }
+}
+
+playAudio()
 skillAnimator()
