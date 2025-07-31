6502866a2b06116f0f609b5c6de69009be130e92
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-07-31 15:48:07 -0600

    CREATED: notes.vb.
    REFACTORED: index.html - Reordered sections. style.css - Adjusted various styles.

---
+++ assets/notes.vb
@@ -0,0 +1,51 @@
+Chrome Devtool Mobile Devices (reverse dimensions for landscape)
+
+375 x 667 iPhone SE
+414 x 896 iPhone XR
+390 x 844 iPhone 12 Pro
+430 x 932 iPhone 14 Pro Max
+412 x 915 Pixel 7
+360 x 740 Samsung Galaxy S8+
+412 x 915 Samsung Galaxy S20 Ultra
+768 x 1024 iPad Mini
+820 x 1180 iPad Air
+1024 x 1366 iPad Pro
+912 x 1368 Surface Pro 7
+540 x 720 Surface Duo (dual screen 1114 x 720)
+344 x 882 (unfolded 690 x 829) Galaxy Z Fold 5
+853 x 1280 (unfolded 1706 x 1280) Asus Zenbook Fold
+412 x 914 Samsung Galaxy A51/71
+1024 x 600 Nest Hub
+1280 x 800 Nest Hub Max
+
+
+1706
+1368
+1280
+1024
+991.98 (col-lg)
+
+Navbar:
+Make menu items stack vertically on smaller screens when space gets too cramped. Maybe do a slide in effect for the menu?
+
+
+Hero:
+Decide on what to put there (likely scrap the codetree and put something else there that has icons, maybe moving around?)
+Maybe make the hero section the about me section, but without including everything in the about me section? Something just like,
+"Hello, I'm Tim Sharp, Full-Stack Web Developer" and maybe a link/download link to my resume?
+
+
+Skills section:
+Make sure the toolbox has some indication that it should be clicked on (maybe for the about me pic as well?).
+Scrap making each skill logo grow when you hover over it instead just add a progress bar that appears under each skill logo that shows 
+how proficient you are in that skill with a slight delayed effect, appearing after they have come to rest (or maybe just before?).
+
+
+Expand on portfolio section:
+Add/change links to be a live site link and a link to github repo.
+Make project images larger to showcase what you have made.
+Give more information on the project (ie. describe the project a bit?). Maybe have this come up in an overlay on top of the image
+when hovering over the image?
+
+
+Keepr2 - Look into redirecting from the account sign up page to a edit form to update the users nickname so it wont be their email.
